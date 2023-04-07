05add9f250d2ab4741fea7ba98801740106db82d
Aleks_Rulet <ananim-kaka@yandex.ru>
2023-04-07 06:58:53 +0300

    Добавить сервис статистики

--- ewm-stats-service/service/src/main/java/ru/practicum/ewm/hit/HitController.java
+++ ewm-stats-service/service/src/main/java/ru/practicum/ewm/hit/HitController.java
@@ -12,7 +12,7 @@ import javax.validation.Valid;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/hit")
+@RequestMapping(path = "/hit")
 public class HitController {
     private final HitService hitService;
 
