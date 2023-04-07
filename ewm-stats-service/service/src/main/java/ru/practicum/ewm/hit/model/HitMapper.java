12f0abfdac17cc2a593535ba2b5d966c50245935
Aleks_Rulet <ananim-kaka@yandex.ru>
2023-04-07 06:18:29 +0300

    Добавить сервис статистики

--- ewm-stats-service/service/src/main/java/ru/practicum/ewm/hit/model/HitMapper.java
+++ ewm-stats-service/service/src/main/java/ru/practicum/ewm/hit/model/HitMapper.java
@@ -5,7 +5,6 @@ import ru.practicum.ewm.dto.HitDto;
 public class HitMapper {
     public static Hit toHit(HitDto hitDto) {
         Hit hit = new Hit();
-        hit.setId(hitDto.getId());
         hit.setApp(hitDto.getApp());
         hit.setUri(hitDto.getUri());
         hit.setIp(hitDto.getIp());
